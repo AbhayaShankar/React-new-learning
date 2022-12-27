34432625dab3111041666e4d2d5c5e9661fe2295
Abhaya <shankarabhaya9@gmail.com>
2022-12-27 15:16:18 +0530

    project done and dusted

--- projects/project-1/src/Interest.js
+++ projects/project-1/src/Interest.js
@@ -6,10 +6,10 @@ export default function Interest() {
     <div className="interest">
       <h4> Interest </h4>
       <p>
-        My name is Abhaya Shankar, an enthusiastic Web developer. I love to
-        create creative looking UI/UX. I am currently learning React and
-        building apps. Open to colaborate and help others starting out on
-        Web-Dev.
+        I enjoy Problem solving. Eliminating a real time problem PIQUES MY
+        INTEREST. Other than that I lovedDeveloping User Interfaces for Websites
+        and Personal Projects. Also I love playing outdoor games and I never
+        miss an opportunity to a sports Battle 😉.
       </p>
     </div>
   );
