6db7e9aafe5d49bab564e81f340d3bbf0df220c3
Abhaya <shankarabhaya9@gmail.com>
2023-01-08 22:17:15 +0530

    state started

--- projects/Project-4/meme/src/Form.js
+++ projects/Project-4/meme/src/Form.js
@@ -1,33 +1,36 @@
 import React from "react";
-import useState from "react";
 import image from "./Images/image.png";
 import memesdata from "./memesdata";
 
 export default function Form() {
+  const [memeUrl, setMemeUrl] = React.useState(
+    "https://i.imgflip.com/4xgqu.jpg"
+  );
+
   function getMemeImage() {
-    const result = { useState };
-    console.log(result);
     const memeArray = memesdata.data.memes;
     const memeIndex = Math.floor(Math.random() * memeArray.length);
-    const url = memeArray[memeIndex].url;
-    console.log(url);
-    const memeImage = document.getElementById("image-root");
-    console.log(memeImage);
+    const newUrl = memeArray[memeIndex].url;
+    setMemeUrl(newUrl);
+    console.log(newUrl);
   }
 
   return (
-    <div className="div">
-      <div className="form-container">
-        <input className="input" type="text" placeholder="top text" />
-        <input className="input" type="text" placeholder="bottom text" />
+    <div>
+      <div className="div">
+        <div className="form-container">
+          <input className="input" type="text" placeholder="top text" />
+          <input className="input" type="text" placeholder="bottom text" />
+        </div>
+        <div>
+          <button className="btn" onClick={getMemeImage}>
+            Get a new meme Image{" "}
+            <img className="img-image" src={image} alt="image-png" />{" "}
+          </button>
+          {/* <img id="image-root" src={imgurl} alt="image-meme" /> */}
+        </div>
       </div>
-      <div>
-        <button className="btn" onClick={getMemeImage}>
-          Get a new meme Image{" "}
-          <img className="img-image" src={image} alt="image-png" />{" "}
-        </button>
-        {/* <img id="image-root" src={imgurl} alt="image-meme" /> */}
-      </div>
+      <img className="meme-image" src={memeUrl} alt="meme-image" />
     </div>
   );
 }
