119707972c038f7dc891085ca7404e4de712d1f2
Abhaya <shankarabhaya9@gmail.com>
2023-01-17 14:12:15 +0530

    card designed

--- projects/Buziboard/buziboard/src/Main.jsx
+++ projects/Buziboard/buziboard/src/Main.jsx
@@ -2,29 +2,22 @@ import React from "react";
 import abhaya from "./Images/Abhaya.PNG";
 export default function Main({ data }) {
   return (
-    <div className="card--0">
-      <div className="card">
-        <div className="card-top">
-          <h3>Buziboard</h3>
-          <button className="btn">Get your card</button>
-        </div>
-        <div className="card-main">
-          <div className="card-img">
-            <img src={abhaya} alt="" />
-          </div>
-          <div className="card-details">
-            <h4 className="Name">{data.name}</h4>
-            <h4 className="nickname">({data.username})</h4>
-            <div className="card-email">{data.email}</div>
-            <div className="card-address">
-              <div className="card-address-1">{data.address.street}</div>
-              <div className="card-address-2">{data.address.suite}</div>
-              <div className="card-address-3">{data.address.city}</div>
-            </div>
-            <div className="card-mobile">{data.phone}</div>
-            <div className="card-website">{data.website}</div>
-          </div>
-        </div>
+    <div className="card-0">
+      <div className="card-top"></div>
+      <div className="card-image">
+        <img src={abhaya} alt="photo" />
+      </div>
+      <div className="card-main">
+        <h2 className="card-name">{data.name}</h2>
+        <p className="card-username">({data.username})</p>
+        <p className="card-email">{data.email}</p>
+        <p className="card-address">
+          {data.address.street} , {data.address.city}{" "}
+        </p>
+      </div>
+      <button className="card-more-info">Get your card</button>
+      <div className="card-website">
+        <div className="card-website-name"> {data.website}</div>
       </div>
     </div>
   );
