c6286fadfccde879be0457f1f7193677bdeed8e8
Abhaya <shankarabhaya9@gmail.com>
2023-02-04 20:51:09 +0530

    tenzies game update

--- projects/Tenzies/Tenzies/src/App.jsx
+++ projects/Tenzies/Tenzies/src/App.jsx
@@ -1,23 +1,57 @@
 import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 import Die from "./component/Die";
 // import reactLogo from "./assets/react.svg";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newArray = [];
+    for (let i = 0; i < 10; i++) {
+      newArray.push({
+        value: Math.floor(Math.random() * 6 + 1),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    // console.log(newArray);
+    return newArray;
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+    // console.log(die);
+  }
+
+  const diceValue = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
   return (
     <div>
       <main className="main">
-        <div className="dice-container">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-        </div>
+        <h1 className="tenzies">Tenzies</h1>
+        <br />
+        <p className="text">
+          Roll until all dice are the same. Click each die to freze as it as its
+          current value between rolls.
+        </p>
+        <br />
+        <br />
+        <div className="dice-container">{diceValue}</div>
+        <button onClick={() => setDice(allNewDice)} className="roll">
+          Roll
+        </button>
       </main>
     </div>
   );
--- projects/Tenzies/Tenzies/src/component/Die.jsx
+++ projects/Tenzies/Tenzies/src/component/Die.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
   return (
-    <div className="dice-face">
+    <div className="dice-face" onClick={props.holdDice} style={styles}>
       <h2 className="dice-value">{props.value}</h2>
     </div>
   );
