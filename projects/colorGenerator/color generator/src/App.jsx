d89a023d9357b6ea39c44493010f5a4142a13a07
Abhaya <shankarabhaya9@gmail.com>
2023-02-08 13:23:41 +0530

    color generator started

---
+++ projects/colorGenerator/color generator/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Value from "values.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Abhaya</h1>
+    </div>
+  );
+}
+
+export default App;
