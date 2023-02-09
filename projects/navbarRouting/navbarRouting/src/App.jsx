ec49ef5acbd31b53edff416a529465003dd6922f
Abhaya <shankarabhaya9@gmail.com>
2023-02-09 22:35:48 +0530

    Navbar started

---
+++ projects/navbarRouting/navbarRouting/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
