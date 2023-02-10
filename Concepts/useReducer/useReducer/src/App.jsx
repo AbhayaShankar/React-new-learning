44818c8449a0c9feea20cd3b2abd2ef31bd53729
Abhaya <shankarabhaya9@gmail.com>
2023-02-10 16:37:16 +0530

    useReducer statred

--- Concepts/useReducer/useReducer/src/App.jsx
+++ Concepts/useReducer/useReducer/src/App.jsx
@@ -1,10 +1,20 @@
 import React, { useReducer, useState } from "react";
 
 function App() {
+  const ACTION_TYPES = {
+    INCREMENT: "increment-age",
+    DECREMENT: "decrement-age",
+  };
+
   function reducer(state, action) {
-    if (action.type === "increment-age") {
+    if (action.type === ACTION_TYPES.INCREMENT) {
       return {
         age: state.age + 1,
+      };
+    }
+    if (action.type === ACTION_TYPES.DECREMENT) {
+      return {
+        age: state.age - 1,
       };
     } else {
       throw new Error("unknown action");
@@ -17,10 +27,17 @@ function App() {
     <div className="App">
       <button
         onClick={() => {
-          dispatch({ type: "increment-age" });
+          dispatch({ type: ACTION_TYPES.INCREMENT });
         }}
       >
         Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.DECREMENT });
+        }}
+      >
+        Decrement
       </button>
       <p>hey! you are {state.age} years old</p>
     </div>
