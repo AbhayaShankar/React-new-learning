915aa22248950bf0ed7bc27d6073bd850c07f65b
Abhaya <shankarabhaya9@gmail.com>
2023-02-11 20:09:02 +0530

    build created

--- projects/navbarRouting/navbarRouting/src/App.jsx
+++ projects/navbarRouting/navbarRouting/src/App.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 
 export default function App() {
+  // Here we built a Navbar with responsive design and when the display width is less than 800px then a hamburger icon appears and animated navbar is viewed.
+
+  // We used useState and useRef to build this navbar. A great project. Simple yet tricky, good to develop a hands-on on the topic and getting started.
   return (
     <div className="App">
       <Navbar />
