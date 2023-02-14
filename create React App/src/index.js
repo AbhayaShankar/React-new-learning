3ea55c571611018085cb5a47dc8ff9a52406701d
Abhaya <shankarabhaya9@gmail.com>
2023-02-14 19:07:49 +0530

    files chamged

--- create React App/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Navbar from "./Components/Navbar";
-import Maincontent from "./Components/Maincontent";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
